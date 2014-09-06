75e49f3ef4cb8a7ccaa3cd43f9b881ce9fbb5a01
YoshiShaPow <yoshipga@gmail.com>
2014-09-06 11:51:39 -0700

    Revert "s2w: Use s2s-only (s2w_switch == 2) properly"
    This reverts commit 8a656ea5accfbc81d7d87d2168fa1e8f890d6b45.
    
    Conflicts:
    	drivers/input/touchscreen/synaptics_dsx_i2c.c

--- drivers/input/touchscreen/synaptics_dsx_i2c.c
+++ drivers/input/touchscreen/synaptics_dsx_i2c.c
@@ -3697,7 +3697,7 @@ static int synaptics_rmi4_suspend(struct device *dev)
 			rmi4_data->board;
 
 #ifdef CONFIG_TOUCHSCREEN_PREVENT_SLEEP
-	if (s2w_switch == 1 || dt2w_switch)
+	if (s2w_switch > 0 || dt2w_switch > 0) 
 	{
 		if (prox_covered) {
 #endif
