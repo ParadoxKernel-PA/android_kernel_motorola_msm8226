afc6a4581664e532aefe0041ca6ac59c101fb7a4
YoshiShaPow <yoshipga@gmail.com>
2014-09-06 00:46:26 -0700

    smartassfalcon: initial push

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -390,6 +390,9 @@ extern struct cpufreq_governor cpufreq_gov_badass;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DANCEDANCE)
 extern struct cpufreq_governor cpufreq_gov_dancedance;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_dancedance)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASSFALCON)
+extern struct cpufreq_governor cpufreq_gov_smartass_falcon;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_smartass_falcon)
 #endif
 
 
